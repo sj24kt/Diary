2d6b5730ade774722f06fde5e46d05d8fe685001
Sherrie Jones <sj24kt@comcast.net>
2015-02-26 17:47:56 -0700

    Initial Commit

---
+++ Diary/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Diary
+//
+//  Created by Sherrie Jones on 2/26/15.
+//  Copyright (c) 2015 Sherrie Jones. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
